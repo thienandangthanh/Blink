e77b75c0af8e141ebe853ceb5092ad93d153fa09
TADT1909 <thienandangthanh@gmail.com>
2017-01-26 20:43:05 +0700

    Extend some functions

--- Blink.cpp
+++ Blink.cpp
@@ -5,8 +5,15 @@
 Blink::Blink(byte _LEDpin, long _onTime, long _offTime)
 {
 	LEDpin = _LEDpin;
+	pinMode(LEDpin, OUTPUT);
 	onTime = _onTime;
 	offTime = _offTime;
+	LEDstate = LOW;
+	previousMillis = 0;
+}
+Blink::Blink(byte _LEDpin)
+{
+	LEDpin = _LEDpin;
 	pinMode(LEDpin, OUTPUT);
 	LEDstate = LOW;
 	previousMillis = 0;
@@ -27,5 +34,65 @@ void Blink::update()
 		LEDstate = HIGH;  // turn it on
 		previousMillis = currentMillis;   // Remember the time
 		digitalWrite(LEDpin, LEDstate);   // Update the actual LED
+	}
+}
+void Blink::update(long _onTime, long _offTime)
+{
+	onTime = _onTime;
+	offTime = _offTime;
+	unsigned long currentMillis = millis();
+	if ((LEDstate == HIGH) && (currentMillis - previousMillis >= onTime))
+	{
+		LEDstate = LOW;  // Turn it off
+		previousMillis = currentMillis;  // Remember the time
+		digitalWrite(LEDpin, LEDstate);  // Update the actual LED
+	}
+	else if ((LEDstate == LOW) && (currentMillis - previousMillis >= offTime))
+	{
+		LEDstate = HIGH;  // turn it on
+		previousMillis = currentMillis;   // Remember the time
+		digitalWrite(LEDpin, LEDstate);   // Update the actual LED
+	}
+}
+void Blink::on()
+{
+	LEDstate = HIGH;
+	digitalWrite(LEDpin, LEDstate);  // Update the actual LED
+}
+void Blink::on(long _onTime)
+{
+	onTime = _onTime;
+	unsigned long currentMillis = millis();
+	if ((LEDstate == LOW) && (currentMillis - previousMillis <= onTime))
+	{
+		LEDstate = HIGH;  // Turn it on
+		previousMillis = currentMillis;  // Remember the time
+		digitalWrite(LEDpin, LEDstate);  // Update the actual LED
+	}
+	else
+	{
+		LEDstate = LOW;
+		digitalWrite(LEDpin, LEDstate);  // Update the actual LED
+	}
+}
+void Blink::off()
+{
+	LEDstate = LOW;
+	digitalWrite(LEDpin, LEDstate);  // Update the actual LED
+}
+void Blink::off(long _offTime)
+{
+	offTime = _offTime;
+	unsigned long currentMillis = millis();
+	if ((LEDstate == LOW) && (currentMillis - previousMillis <= offTime))
+	{
+		LEDstate = LOW;  // Turn it off
+		previousMillis = currentMillis;  // Remember the time
+		digitalWrite(LEDpin, LEDstate);  // Update the actual LED
+	}
+	else
+	{
+		LEDstate = HIGH;
+		digitalWrite(LEDpin, LEDstate);  // Update the actual LED
 	}
 }
--- Blink.h
+++ Blink.h
@@ -1,16 +1,24 @@
+// Blink Library version 2.0
+// extend some functions
 #ifndef Blink_h
 #define Blink_h
 
 #include "Arduino.h"
 class Blink {
 public:
-	Blink(byte LEDpin, long onTime, long offtime);
+	Blink(byte _LEDpin, long _onTime, long _offTime);
+	Blink(byte _LEDpin);
 	void update();
+	void update(long _onTime, long _offTime);
+	void on();
+	void on(long _onTime);
+	void off();
+	void off(long _offTime);
 
 private:
-	byte LEDpin;
-	long onTime;
-	long offTime;
+	byte LEDpin, _LEDpin;
+	long onTime, _onTime;
+	long offTime, _offTime;
 	boolean LEDstate;
 	unsigned long previousMillis;
 	unsigned long currentMillis = millis();
